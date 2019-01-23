6bc08d0b81fc553002c5c6cd75e7646d261535cb
Leonardo Souza <leogsouza@gmail.com>
2019-01-23 00:02:32 -0200

    Making simple salary calculation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    salary: null,
+    result: null
+  }
+
+  handleChange = (event) => {
+
+    this.setState({
+      salary: event.target.value
+    })    
+  }
+
+  handleCalculate = () => {
+    const salary = this.state.salary;
+
+    const result = salary / 12;
+
+    this.setState({
+      result: result
+    });
+
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <p>
+          Salary Annual Value:
+          <input type="text" onChange={this.handleChange} />
+          <button onClick={this.handleCalculate}>Calculate</button>
+        </p>
+        <p>{this.state.result}</p>
+        
       </div>
     );
   }
