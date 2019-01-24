df23cac53dee5aa8a482e039d60bcc92ec5d2a44
Leonardo Souza <leogsouza@gmail.com>
2019-01-23 22:25:34 -0200

    Add currency format on salary field

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import NavBar from './components/Navbar/Navbar';
+
+import NumberFormat from "react-number-format";
 import './App.css';
 
 class App extends Component {
@@ -15,6 +18,14 @@ class App extends Component {
     })    
   }
 
+  handleValueChange = (values) => {
+    const {formattedValue, value} = values;
+    // formattedValue = $2,223
+    // value ie, 2223
+    console.log(value, formattedValue);
+    this.setState({salary: value})
+  }
+
   handleCalculate = () => {
     const salary = this.state.salary;
 
@@ -29,12 +40,20 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <NavBar />
         <p>
           Salary Annual Value:
-          <input type="text" onChange={this.handleChange} />
+          <NumberFormat thousandSeparator={true} prefix={'$'} onValueChange={this.handleValueChange} />
           <button onClick={this.handleCalculate}>Calculate</button>
         </p>
-        <p>{this.state.result}</p>
+        <p>
+          <NumberFormat 
+            value={this.state.result} 
+            thousandSeparator={true} 
+            displayType={'text'} 
+            prefix={'$'}
+            decimalScale={2}
+            fixedDecimalScale={true} /></p>
         
       </div>
     );
