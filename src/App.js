fc73d1a2ddaefc1f65727f660f96853b837b75e3
Leonardo Gonçalves <ldsz@gft.com>
2020-02-24 18:00:17 -0300

    Calculate salary by type

--- src/App.js
+++ src/App.js
@@ -1,128 +1,126 @@
-import { Button, Col, Row } from 'antd';
+import { Button, Col, Row, Radio } from 'antd';
 import Axios from "axios";
-import React, { Component } from "react";
+import React, { useState } from "react";
 import NumberFormat from "react-number-format";
 import "./App.css";
 import Header from "./components/Header/Header";
 import NavBar from "./components/Navbar/Navbar";
 
-class App extends Component {
-  state = {
-    annual: 0,
-    monthly: 0,
-    converted: 0,
-    calculated: 0
+const App = () => {
+  const [value, setValue] = useState(0);
+  const [annual, setAnnual] = useState(0);
+  const [monthly, setMonthly] = useState(0);
+  const [converted, setConverted] = useState(0);
+  const [calculated, setCalculated] = useState(0);
+  const [type, setType] = useState('annual');
+
+  const handleChange = event => {
+    setValue(event.target.value);
   };
 
-  handleChange = event => {    
-    this.setState({
-      annual: event.target.value
-    });
+  const handleTypeChange = event => {
+    setType(event.target.value);
   };
 
-  handleValueChange = values => {
-    this.setState({ annual: values.value });
+  const handleCalculate = () => {
+    currencyConverterHandler(value);    
   };
 
-  handleCalculate = () => {    
-    const salary = this.state.annual;
-
-    this.currencyConverterHandler(salary);
-    
-  };
-
-  currencyConverterHandler = amount => {
+  const currencyConverterHandler = amount => {
     const url = process.env.REACT_APP_CURRENCY_API_URL;
     const params = {
       from: "USD",
       to: "BRL",
-      type: "annual",
+      type: type,
       amount: amount
     };
     const queryString = new URLSearchParams(params).toString();
     Axios.get(`${url}/salary/calculator?${queryString}`).then(response => {
-      console.log('response', response);
-      this.setState({
-        annual: response.data.annual_salary,
-        monthly: response.data.monthly_salary,
-        converted: response.data.converted_salary,
-        calculated: response.data.calculated_salary
-      });
+      setAnnual(response.data.annual_salary);
+      setMonthly(response.data.monthly_salary);
+      setConverted(response.data.converted_salary);
+      setCalculated(response.data.calculated_salary);
     });
   };
 
-  render() {
-    return (
-      <React.Fragment>
-          <div className="App">
-            <Header>
-            <NavBar />
-            </Header>
-              <Row gutter={[0,16]}>
-                <Col span={24}>
-                <NumberFormat
-                  className="ant-input"
-                  style={{width: '15em'}}
-                  placeholder="Enter your salary"
-                  getInputRef = {(el) => this.inputElem = el}
-                  onValueChange={values => {
-                    this.handleChange({
-                      target: {
-                        value: values.value
-                      }
-                    });
-                  }}
-                  thousandSeparator
-                  prefix="$"
-                />
-                </Col>
-              </Row>
-              <Row gutter={[0,16]}>
-                <Col span={24}>
-                <Button
-                  className="calculate-btn"
-                  onClick={this.handleCalculate}
-                >
-                  Calculate
-                </Button>
-                </Col>
-              </Row>
-              <Row gutter={[0,24]}>
-                <Col span={24}>
-                <label>Salary Monthly: </label><NumberFormat
-                value={this.state.monthly}
-                thousandSeparator={true}
-                displayType={"text"}
-                prefix={"$"}
-                decimalScale={2}
-                fixedDecimalScale={true}
+  return (
+    <React.Fragment>
+        <div className="App">
+          <Header>
+          <NavBar />
+          </Header>
+            <Row gutter={[0,16]}>
+              <Col span={24}>
+                <Radio.Group defaultValue={type} value={type} onChange={handleTypeChange}>
+                  <Radio.Button value="annual">Annual</Radio.Button>
+                  <Radio.Button value="monthly">Monthly</Radio.Button>
+                  <Radio.Button value="daily">Daily</Radio.Button>
+                  <Radio.Button value="hourly">Hourly</Radio.Button>
+                </Radio.Group>
+              </Col>
+            </Row>
+            <Row gutter={[0,16]}>
+              <Col span={24}>
+              <NumberFormat
+                className="ant-input"
+                style={{width: '15em'}}
+                placeholder="Enter your salary"
+                // getInputRef = {(el) => this.inputElem = el}
+                onValueChange={values => {
+                  handleChange({
+                    target: {
+                      value: values.value
+                    }
+                  });
+                }}
+                thousandSeparator
+                prefix="$"
               />
-              <p></p>
-              <label>Salary Converted to: </label><NumberFormat
-                value={this.state.converted}
-                thousandSeparator={true}
-                displayType={"text"}
-                prefix={"R$"}
-                decimalScale={2}
-                fixedDecimalScale={true}
-              />
-              <p></p>
-              <label>Brazilian Salary: </label><NumberFormat
-                value={this.state.calculated}
-                thousandSeparator={true}
-                displayType={"text"}
-                prefix={"R$"}
-                decimalScale={2}
-                fixedDecimalScale={true}
-              />
-                </Col>
-              </Row>
-          </div>
-      </React.Fragment>
-    );
-  }
+              </Col>
+            </Row>
+            <Row gutter={[0,16]}>
+              <Col span={24}>
+              <Button
+                className="calculate-btn"
+                onClick={handleCalculate}
+              >
+                Calculate
+              </Button>
+              </Col>
+            </Row>
+            <Row gutter={[0,24]}>
+              <Col span={24}>
+              <label>Salary Monthly: </label><NumberFormat
+              value={monthly}
+              thousandSeparator={true}
+              displayType={"text"}
+              prefix={"$"}
+              decimalScale={2}
+              fixedDecimalScale={true}
+            />
+            <p></p>
+            <label>Salary Converted to: </label><NumberFormat
+              value={converted}
+              thousandSeparator={true}
+              displayType={"text"}
+              prefix={"R$"}
+              decimalScale={2}
+              fixedDecimalScale={true}
+            />
+            <p></p>
+            <label>Brazilian Salary: </label><NumberFormat
+              value={calculated}
+              thousandSeparator={true}
+              displayType={"text"}
+              prefix={"R$"}
+              decimalScale={2}
+              fixedDecimalScale={true}
+            />
+              </Col>
+            </Row>
+        </div>
+    </React.Fragment>
+  );
 }
-
-
 
 export default App;
