eb7d4a05ba76089347c804b0b4d0fa33bf26d578
Leonardo Souza <leogsouza@gmail.com>
2021-05-26 21:22:01 -0300

    Replace currency component and added select currency

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import { Button, Col, Row, Radio, InputNumber } from 'antd';
+import { Button, Col, Row, Radio, InputNumber, Select } from 'antd';
 import Axios from "axios";
 import React, { useState } from "react";
 import NumberFormat from "react-number-format";
+
 import "./App.css";
 import Header from "./components/Header/Header";
 import NavBar from "./components/Navbar/Navbar";
+
+import currencyList from './currencyList';
+
+const { Option } = Select;
+const locale = 'en-us';
+const currencyOptions = currencyList.data.map(c => ({
+  label: c.name,
+  value: c.code,
+  symbol: c.symbol
+}));
+
+
 
 const App = () => {
   const [value, setValue] = useState(0);
@@ -15,6 +28,7 @@ const App = () => {
   const [type, setType] = useState('annual');
   const [hours, setHours] = useState(8)
   const [showHours, setShowHours] = useState(false);
+  const [currency, setCurrency] = useState(currencyOptions[0].value)
 
   const handleChange = event => {
     setValue(event.target.value);
@@ -36,10 +50,49 @@ const App = () => {
     setHours(value)
   }
 
+
+  const currencyFormatter = selectedCurrency => value =>{
+    return new Intl.NumberFormat(locale, {
+      style: 'currency',
+      currency: selectedCurrency
+    }).format(value);
+  }
+  
+  const currencyParser = val => {
+    try {
+      if (typeof val === 'string' && !val.length) {
+        val = '0.0';
+      }
+  
+      let group = new Intl.NumberFormat(locale).format(1111).replace(/1/g, "");
+      let decimal = new Intl.NumberFormat(locale).format(1.1).replace(/1/g, "");
+      let reversedVal = val.replace(new RegExp("\\" + group, "g"), "");
+      reversedVal = reversedVal.replace(new RegExp("\\" + decimal, "g"), ".");
+  
+  
+      reversedVal = reversedVal.replace(/[^0-9.]/g, "");
+  
+      const digitsAfterDecimalCount = (reversedVal.split(".")[1] || []).length;
+      const needsDigitsAppend = digitsAfterDecimalCount > 2;
+  
+      if (needsDigitsAppend) {
+        reversedVal = reversedVal * Math.pow(10, digitsAfterDecimalCount -2);
+      }
+      console.log(reversedVal);
+      setValue(reversedVal);
+      return Number.isNaN(reversedVal) ? 0 : reversedVal;
+  
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
+
   const currencyConverterHandler = amount => {
     const url = process.env.REACT_APP_CURRENCY_API_URL;
     const params = {
-      from: "USD",
+      from: currency,
       to: "BRL",
       type: type,
       amount: amount
@@ -73,28 +126,28 @@ const App = () => {
                   <Radio.Button value="hourly">Hourly</Radio.Button>
                 </Radio.Group>
               </Col>
-            </Row>
-            <Row gutter={[0,16]}>
               <Col span={24}>
-              <NumberFormat
-                className="ant-input"
-                style={{width: '15em'}}
-                placeholder="Enter your salary"
-                // getInputRef = {(el) => this.inputElem = el}
-                onValueChange={values => {
-                  handleChange({
-                    target: {
-                      value: values.value
-                    }
-                  });
-                }}
-                thousandSeparator
-                prefix="$"
-              />
+              <Select
+                  showSearch
+                  value={currency}
+                  style={{ width: '7rem', marginTop: '1rem',  marginRight: '1rem'}}
+                  onChange={setCurrency}
+                >
+                  {currencyOptions.map(opt => (
+                    <Option key={opt.value} value={opt.value}>
+                      {opt.label}
+                    </Option>
+                  ))}
+                </Select>
+                <InputNumber
+                  defaultValue={0.00}
+                  style={{width: '15em'}}
+                  formatter={currencyFormatter(currency)}
+                  parser={currencyParser}
+                />               
+              
               </Col>
-            </Row>
-            <Row gutter={[0,16]} style={{display: showHours ? 'block' : 'none' }}>
-              <Col span={24}>
+              <Col span={24} style={{display: showHours ? 'block' : 'none' }}>
                 <label>Hours per day: </label><InputNumber
                   placeholder="Hours por day"
                   defaultValue={8}
@@ -102,8 +155,6 @@ const App = () => {
                   onChange={handleHoursChange}
                 />
               </Col>
-            </Row>
-            <Row gutter={[0,16]}>
               <Col span={24}>
               <Button
                 className="calculate-btn"
@@ -112,8 +163,6 @@ const App = () => {
                 Calculate
               </Button>
               </Col>
-            </Row>
-            <Row gutter={[0,24]}>
               <Col span={24}>
               <label>Salary Monthly: </label><NumberFormat
               value={monthly}
---
+++ src/currencyList.js
@@ -0,0 +1,24 @@
+export default {
+    data: [
+        {
+            name: 'US Dollar', 
+            code: 'USD',
+            symbol: '$',
+        },
+        {
+            name: 'Euro', 
+            code: 'EUR',
+            symbol: '€'
+        },
+        {
+            name: 'British Pound',
+            code: 'GBP',
+            symbol: '£',
+        },
+        {
+            name: 'Brazilian Real',
+            code: 'BRL',
+            symbol: 'R$',
+        }
+    ]
+}
